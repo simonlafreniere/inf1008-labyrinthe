f682f07edc062c58c2e73562b1152a67127a3725
Simon <simon.lafreniere@uqtr.ca>
2018-03-21 10:43:31 -0400

    ajout test

---
+++ labyrinthe/CGraphe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labyrinthe
+{
+    class CGraphe
+    {
+        private int[,] graphe;
+        private int[] visites;
+        private int largeur, hauteur, poidsMax, dimension;
+
+        public CGraphe(int largeur, int hauteur, int poids)
+        {
+            this.largeur = largeur;
+            this.hauteur = hauteur;
+            poidsMax = poids;
+            initGraphe();
+        }
+
+        public void initGraphe()
+        {
+            dimension=this.largeur * this.hauteur;
+            graphe = new int[dimension,2];
+            Random rdn = new Random();
+            for(int i = 0; i < dimension; i++)
+            {
+                graphe[i, 1] = rdn.Next(1, poidsMax + 1);
+                graphe[i, 2] = rdn.Next(1, poidsMax + 1);
+            }
+
+            visites= new int[dimension];
+        }
+    }
+}
